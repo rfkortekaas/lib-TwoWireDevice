7a13c3b476c954cba607969db60548da5e024cb4
Tijs van Roon <knifter@gmail.com>
2018-11-25 15:54:13 +0100

    TwoWire object as reference

--- src/TwoWireDevice.cpp
+++ src/TwoWireDevice.cpp
@@ -2,19 +2,10 @@
 
 #include "Arduino.h"
 #include <stdio.h>
-//#include <math.h>
-
-TwoWireDevice::TwoWireDevice(TwoWire *wire, const uint8_t addr) : _i2caddr(addr), _wire(wire)
-{
-}
-
-TwoWireDevice::TwoWireDevice(const uint8_t addr) : _i2caddr(addr), _wire(&Wire)
-{
-}
 
 bool TwoWireDevice::begin()
 {
-    _wire->begin();
+    _wire.begin();
     return true;
 }
 
@@ -30,14 +21,6 @@ void TwoWireDevice::write8(const uint8_t reg, const uint8_t value)
 	write(reg, &value, 1);
 }
 
-// void BME280_I2C::write8(uint8_t reg, uint8_t value)
-// {
-//     _wire -> beginTransmission((uint8_t)_i2caddr);
-//     _wire -> write((uint8_t)reg);
-//     _wire -> write((uint8_t)value);
-//     _wire -> endTransmission();
-// }
-
 /**************************************************************************/
 /*!
     @brief  read one byte of data from the specified register
@@ -52,39 +35,30 @@ uint8_t TwoWireDevice::read8(const uint8_t reg)
 
 	return ret;
 }
-// uint8_t BME280_I2C::read8(uint8_t reg) {
-//     _wire -> beginTransmission((uint8_t)_i2caddr);
-//     _wire -> write((uint8_t)reg);
-//     _wire -> endTransmission();
-//     _wire -> requestFrom((uint8_t)_i2caddr, (uint8_t)1);
-//
-//     return _wire -> read();
-// }
 
 uint16_t TwoWireDevice::read16(const uint8_t reg)
 {
-    _wire -> beginTransmission(_i2caddr);
-    _wire -> write(reg);
-    _wire -> endTransmission();
-    _wire -> requestFrom(_i2caddr, (uint8_t)2);
-    return ((_wire -> read() << 8) | _wire -> read());
+    _wire.beginTransmission(_i2caddr);
+    _wire.write(reg);
+    _wire.endTransmission();
+    _wire.requestFrom(_i2caddr, (uint8_t)2);
+    return ((_wire.read() << 8) | _wire.read());
 }
 
 uint32_t TwoWireDevice::read24(const uint8_t reg)
 {
     uint32_t value;
 
-    _wire -> beginTransmission(_i2caddr);
-    _wire -> write(reg);
-    _wire -> endTransmission();
-    _wire -> requestFrom(_i2caddr, (uint8_t)3);
+    _wire.beginTransmission(_i2caddr);
+    _wire.write(reg);
+    _wire.endTransmission();
+    _wire.requestFrom(_i2caddr, (uint8_t)3);
 
-    value = _wire->read();
+    value = _wire.read();
     value <<= 8;
-    value |= _wire->read();
+    value |= _wire.read();
     value <<= 8;
-    //if(_wire)
-    value |= _wire->read();
+    value |= _wire.read();
 
     return value;
 }
@@ -97,14 +71,14 @@ void TwoWireDevice::read(const uint8_t reg, uint8_t *buf, const uint8_t num)
 	while(pos < num)
     {
 		uint8_t read_now = min(32, (num - pos));
-		_wire->beginTransmission(_i2caddr);
-		_wire->write(reg + pos);
-		_wire->endTransmission();
-		_wire->requestFrom(_i2caddr, read_now);
+		_wire.beginTransmission(_i2caddr);
+		_wire.write(reg + pos);
+		_wire.endTransmission();
+		_wire.requestFrom(_i2caddr, read_now);
 
 		for(int i=0; i<read_now; i++)
         {
-			buf[pos] = _wire->read();
+			buf[pos] = _wire.read();
 			pos++;
 		}
 	}
@@ -112,8 +86,8 @@ void TwoWireDevice::read(const uint8_t reg, uint8_t *buf, const uint8_t num)
 
 void TwoWireDevice::write(const uint8_t reg, const uint8_t *buf, const uint8_t num)
 {
-	_wire->beginTransmission(_i2caddr);
-	_wire->write(reg);
-	_wire->write(buf, num);
-	_wire->endTransmission();
+	_wire.beginTransmission(_i2caddr);
+	_wire.write(reg);
+	_wire.write(buf, num);
+	_wire.endTransmission();
 }
--- src/TwoWireDevice.h
+++ src/TwoWireDevice.h
@@ -9,8 +9,8 @@
 class TwoWireDevice
 {
     public:
-        TwoWireDevice(const uint8_t addr);
-        TwoWireDevice(TwoWire*, const uint8_t addr);
+		TwoWireDevice(TwoWire& wire, const uint8_t addr) : _i2caddr(addr), _wire(wire) {};
+		TwoWireDevice(const uint8_t addr) : _i2caddr(addr), _wire(Wire) {};
 
         bool begin();
 
@@ -19,14 +19,16 @@ class TwoWireDevice
         void read(const uint8_t reg, uint8_t *buf, const uint8_t num);
         void write(const uint8_t reg, const uint8_t *buf, const uint8_t num);
 
-        void      write8(const uint8_t reg, const uint8_t value);
-        void      write16(const uint8_t reg, const uint16_t value);
         uint8_t   read8(const uint8_t reg);
+        void      write8(const uint8_t reg, const uint8_t value);
+
         uint16_t  read16(const uint8_t reg);
+        void      write16(const uint8_t reg, const uint16_t value);
+
         uint32_t  read24(const uint8_t reg);
 
         uint8_t _i2caddr;
-        TwoWire* _wire = &Wire;
+        TwoWire& _wire = Wire;
 };
 
 #endif //__TWOWIREDEVICE_H
